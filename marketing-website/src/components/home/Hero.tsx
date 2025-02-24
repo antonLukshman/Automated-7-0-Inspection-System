04f6b88ab8cc6c237dc15fc2f15931fcd62e5c87
antonLukshman <159231205+antonLukshman@users.noreply.github.com>
2025-02-24 12:12:12 +0530

    Align the Ui mock design

--- marketing-website/src/components/home/Hero.tsx
+++ marketing-website/src/components/home/Hero.tsx
@@ -32,10 +32,10 @@ export const Hero = () => {
           </main>
         </div>
       </div>
-      <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+      <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2 flex items-center justify-center">
         <img
-          className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
-          src="/api/placeholder/800/600"
+          className="h-58 w-48 object-cover" // Adjusted height and width to make the image smaller
+          src="/Mockup.jpg"
           alt="Quality inspection in action"
         />
       </div>
